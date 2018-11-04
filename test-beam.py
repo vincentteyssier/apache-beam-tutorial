9eb6a4062d909a2598e4bcd1e4f62b2429d9b4cc
Vincent Teyssier <v.teyssier@gmail.com>
2018-11-04 11:33:06 +0100

    Fixes. Still wrong grouping, work in progress

--- test-beam.py
+++ test-beam.py
@@ -1,6 +1,5 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-import numpy as np
 
 # defining custom arguments
 class MyOptions(PipelineOptions):    
@@ -14,16 +13,30 @@ class MyOptions(PipelineOptions):
                         default='./output/')
 
 # class to split a csv line by elements 
-class Split(apache_beam.DoFn):
+class Split(beam.DoFn):
     def process(self, element):
         Date,Open,High,Low,Close,Volume = element.split(",")
         return [{
+            'Date': Date,
             'Open': float(Open),
             'Close': float(Close)
         }]
 
+
+class CollectOpen(beam.DoFn):
+    def process(self, element):
+        # Returns a list of tuples containing Date and Open value
+        result = [(element['Date'], element['Open'])]
+        return result
+
+class CollectClose(beam.DoFn):
+    def process(self, element):
+        # Returns a list of tuples containing Date and Open value
+        result = [(element['Date'], element['Close'])]
+        return result
+
 # class to calculate the standard deviation over an entire PCollection
-class Standard_deviation(apache_beam.DoFn):
+class Standard_deviation(beam.DoFn):
     def create_accumulator(self):
         return (0.0, 0.0, 0) # x, x^2, count
 
@@ -38,51 +51,66 @@ class Standard_deviation(apache_beam.DoFn):
     def extract_output(self, sum_count):
         (sum, sumsq, count) = sum_count
         if count:
-        mean = sum / count
-        variance = (sumsq / count) - mean*mean
-        # -ve value could happen due to rounding
-        stddev = np.sqrt(variance) if variance > 0 else 0
-        return {
-            'mean': mean,
-            'variance': variance,
-            'stddev': stddev,
-            'count': count
-        }
+            mean = sum / count
+            variance = (sumsq / count) - mean*mean
+            stddev = np.sqrt(variance) if variance > 0 else 0
+            return {
+                'mean': mean,
+                'variance': variance,
+                'stddev': stddev,
+                'count': count
+            }
         else:
-        return {
-            'mean': float('NaN'),
-            'variance': float('NaN'),
-            'stddev': float('NaN'),
-            'count': 0
-        }
+            return {
+                'mean': float('NaN'),
+                'variance': float('NaN'),
+                'stddev': float('NaN'),
+                'count': 0
+            }
 
 # setting input and output files
-input_filename = "./data/sp500.csv"
-output_filename = "./output/result.txt"
+input_filename = "c:/tmp/sp500.csv"
+output_filename = "c:/tmp/output/result.txt"
 
 # instantiate the pipeline
 options = PipelineOptions()
-p = beam.Pipeline(options=options)
 
-# reading the csv and splitting lnies by elements we want to retain
-csv_lines = (
-        p | beam.io.ReadFromText(input_filename) |
-        beam.ParDo(Split())
-    )
-
-# calculate the mean
-mean = (
-    csv_lines | "Calculating mean" >> beam.CombineValues(
-        beam.combiners.MeanCombineFn()
+with beam.Pipeline(options=options) as p:
+    # reading the csv and splitting lnies by elements we want to retain
+    csv_lines = (
+            p | beam.io.ReadFromText(input_filename, skip_header_lines=1) |
+            beam.ParDo(Split())
         )
+
+    # calculate the mean for Open values
+    mean_open = (
+        csv_lines | beam.ParDo(CollectOpen()) |
+        "Grouping keys Open" >> beam.GroupByKey() |
+        "Calculating mean for Open" >> beam.CombineValues(
+            beam.combiners.MeanCombineFn()
+            )
+        )
+
+    # calculate the mean for Close values
+    mean_close = (
+        csv_lines | beam.ParDo(CollectClose()) |
+        "Grouping keys Close" >> beam.GroupByKey() |
+        "Calculating mean for Close" >> beam.CombineValues(
+            beam.combiners.MeanCombineFn()
+            )
+        )
+
+    # writing results to file
+    output= ( 
+        { 
+            'Mean Open': mean_open,
+            'Mean Close': mean_close 
+        } | 
+        beam.CoGroupByKey() | 
+        beam.io.WriteToText(output_filename)
     )
 
-# calculate the standard deviation
-std = (
-    csv_lines | "Calculating standard deviation" >> beam.ParDo(Standard_deviation())
-    )
-
-# writing results to file
-output = (
-    mean | beam.io.WriteToText(output_filename)
-)
+print ('Mean Open ') 
+print (mean_open)
+print ('\nMean Close ')
+print(mean_close)
